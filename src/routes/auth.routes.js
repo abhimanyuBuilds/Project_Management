cb7e297c1ea274c40f962bcfbcce6ef67ecca43c
Abhimanyu Singh <158279512+abhimanyuBuilds@users.noreply.github.com>
2026-07-05 13:12:40 +0530

    Add rate limiting to routes

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,15 +1,17 @@
 import Router from "express"
-import { registerUser , userLogin , logOutUser , getCurrentUser , verifyEmail } from "../controllers/auth.controller.js"
-import {validate} from "../middlewares/validation.middleware.js"
-import {userRegisterValidation , userLoginValidation} from "../validations/auth.validation.js"
-import {verifyJWT} from "../middlewares/auth.middleware.js"
+import { registerUser, userLogin, logOutUser, getCurrentUser, verifyEmail } from "../controllers/auth.controller.js"
+import { validate } from "../middlewares/validation.middleware.js"
+import { userRegisterValidation, userLoginValidation } from "../validations/auth.validation.js"
+import { verifyJWT } from "../middlewares/auth.middleware.js"
+import {
+    apiLimiter,
+    loginLimiter,
+    registerLimitter,
+    forgotPasswordRateLimiter,
+    verifyEmailLimiter
+} from "../middlewares/rateLimit.middleware.js"
 
 const router = Router()
-
-
-
-
-
 
 
 /**
@@ -127,10 +129,17 @@ const router = Router()
  *               statusCode: 500
  *               message: Something went wrong while registering a user
  */
-router.route("/register").post(validate(userRegisterValidation) , registerUser )
 
-router.route("/login").post(validate(userLoginValidation) , userLogin)
-router.route("/logout").post(verifyJWT , logOutUser)
-router.route("/verify-email").post(verifyJWT , verifyEmail)
+
+
+
+router.route("/register").post(validate(userRegisterValidation), registerLimitter , registerUser)
+
+router.route("/login").post(validate(userLoginValidation), loginLimiter,userLogin)
+router.route("/logout").post(verifyJWT, logOutUser)
+router.route("/verify-email").post(verifyJWT, verifyEmailLimiter, verifyEmail)
 
 export default router
+
+
+    
